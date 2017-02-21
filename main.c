9f4a38ca95ec7a0925603e4b87de0a10a957219c
Aapo Malkamäki <aapo.malkamaki@helsinki.fi>
2017-02-21 09:23:04 +0200

    making progress, but no success yet

--- main.c
+++ main.c
@@ -15,13 +15,15 @@ int main(int argc, char *argv[]) {
 void main1() {
 	char *filename1 = "turtle.ppm";
 	char *filename2 = "turtle_stereogram.ppm";
+	
 	PICTURE *pic = read_picture(filename1);
 	PICTURE *pic2;
 	if (pic == NULL) {
 		return;
 	}
 	pic2 = depthmap2stereogram(pic, 200, 100, random);
+	
+	print_picture(pic2, filename2);
 	free_picture(pic);
-	print_picture(pic, filename2);
 	free_picture(pic2);
 }
--- make_stereogram.c
+++ make_stereogram.c
@@ -8,20 +8,25 @@
 PICTURE* depthmap2stereogram(PICTURE *depthmap, int maxdepth, int mindepth, PIXEL* (*initial_pixels)(int, int)) {
 	PICTURE *stereogram = create_picture(depthmap->x+maxdepth, depthmap->y);
 	int x,y;
+	printf("%d %d\n", stereogram->x, stereogram->y);
 	
-	for (y=0; y<stereogram->y; y++) {
-		for (x=0; x<stereogram->x; x++) {
-			if (x < maxdepth) {
+	for (y=(stereogram->y)-1; y>=0; y--) {
+		for (x=(stereogram->x)-1; x>=0; x--) {
+			if (x < maxdepth || 1) {
 				PIXEL *new = initial_pixels(x, y);
-				(*(stereogram->pic+(y+1)*x))[0] = (*new)[0];
-				(*(stereogram->pic+(y+1)*x))[1] = (*new)[1];
-				(*(stereogram->pic+(y+1)*x))[2] = (*new)[2];
+				(*(stereogram->pic+x+y*stereogram->x))[0] = (*new)[0];
+				(*(stereogram->pic+x+y*stereogram->x))[1] = (*new)[1];
+				(*(stereogram->pic+x+y*stereogram->x))[2] = (*new)[2];
 			} else {
 				PIXEL *new;
-				new = (stereogram->pic+(y+1)*x - depth(depthmap, x, y, maxdepth, mindepth));
-				(*(stereogram->pic+(y+1)*x))[0] = (*new)[0];
-				(*(stereogram->pic+(y+1)*x))[1] = (*new)[1];
-				(*(stereogram->pic+(y+1)*x))[2] = (*new)[2];
+				int dep = depth(depthmap, x, y, maxdepth, mindepth);
+				new = (stereogram->pic+x+y*stereogram->x - dep);
+				(*(stereogram->pic+x+y*stereogram->x))[0] = (*new)[0];
+				(*(stereogram->pic+x+y*stereogram->x))[1] = (*new)[1];
+				(*(stereogram->pic+x+y*stereogram->x))[2] = (*new)[2];
+				if (dep != maxdepth) {
+					printf("depth: %d, x: %d, y: %d\n", dep, x, y);
+				}
 			}
 		}
 	}
@@ -32,9 +37,9 @@ PICTURE* depthmap2stereogram(PICTURE *depthmap, int maxdepth, int mindepth, PIXE
 int depth(PICTURE *depthmap, int x, int y, int maxdepth, int mindepth) {
 	int avg = 0;
 	x -= maxdepth;
-	avg += depthmap->pic[(y+1)*x][0];
-	avg += depthmap->pic[(y+1)*x][1];
-	avg += depthmap->pic[(y+1)*x][2];
+	avg += depthmap->pic[x+y*depthmap->x][0];
+	avg += depthmap->pic[x+y*depthmap->x][1];
+	avg += depthmap->pic[x+y*depthmap->x][2];
 	avg /= 3;
 	return maxdepth - (maxdepth - mindepth) * avg / COLORMAX;
 }
